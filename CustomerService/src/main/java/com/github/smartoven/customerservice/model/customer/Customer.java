2ef000eb049215a68014c27b1e63f510c5cfa686
Yaroslav Panteleev <yarik.panteleev@gmail.com>
2022-08-15 20:46:04 +0300

    Implemented CRUD operations for Customer model

--- CustomerService/src/main/java/com/github/smartoven/customerservice/model/customer/Customer.java
+++ CustomerService/src/main/java/com/github/smartoven/customerservice/model/customer/Customer.java
@@ -20,8 +20,12 @@ public class Customer {
     }
 
     public Customer(Long id, String username, String password) {
-        super();
         this.id = id;
+        this.username = username;
+        this.password = password;
+    }
+
+    public Customer(String username, String password) {
         this.username = username;
         this.password = password;
     }
@@ -36,5 +40,17 @@ public class Customer {
 
     public String getPassword() {
         return password;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
     }
 }
