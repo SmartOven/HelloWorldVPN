57db1439664ef6690a7a4f21c1dfee8666b00d53
Yaroslav Panteleev <yarik.panteleev@gmail.com>
2022-08-15 16:21:14 +0300

    Created Customer entity and Customer repository

---
+++ CustomerService/src/main/java/com/github/smartoven/customerservice/model/customer/Customer.java
@@ -0,0 +1,40 @@
+package com.github.smartoven.customerservice.model.customer;
+
+import javax.persistence.*;
+
+@Entity
+public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(name = "username")
+    private String username;
+
+    @Column(name = "password")
+    private String password;
+
+    public Customer() {
+
+    }
+
+    public Customer(Long id, String username, String password) {
+        super();
+        this.id = id;
+        this.username = username;
+        this.password = password;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}
