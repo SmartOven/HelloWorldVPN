24b36467295372ab6224bca127c7a40c23e2d451
SmartOven <yarik.panteleev@gmail.com>
2022-10-09 17:28:01 +0300

    Initialized spring boot app

---
+++ bot/src/test/java/com/github/smartoven/bot/BotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.smartoven.bot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
