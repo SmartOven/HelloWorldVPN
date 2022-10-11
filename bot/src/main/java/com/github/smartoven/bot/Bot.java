d1df6f6f8c6ff7316a22affea2aa2abd245e08ce
SmartOven <yarik.panteleev@gmail.com>
2022-10-11 20:35:25 +0300

    Implemented logging

--- bot/src/main/java/com/github/smartoven/bot/Bot.java
+++ bot/src/main/java/com/github/smartoven/bot/Bot.java
@@ -1,29 +1,33 @@
 package com.github.smartoven.bot;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Chat;
+import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
+@Slf4j
 @Component
 public class Bot extends TelegramLongPollingBot {
 
     @Value("${bot.username}")
-    private String username;
+    private String botUsername;
 
     @Value("${bot.token}")
-    private String token;
+    private String botToken;
 
     @Override
     public String getBotUsername() {
-        return username;
+        return botUsername;
     }
 
     @Override
     public String getBotToken() {
-        return token;
+        return botToken;
     }
 
     @Override
@@ -37,15 +41,40 @@ public class Bot extends TelegramLongPollingBot {
 
         String text = update.getMessage().getText();
         long chatId = update.getMessage().getChatId();
+        String username = update.getMessage().getChat().getUserName();
 
+        if (text.contains("/info")) {
+            sendInfo(chatId, update);
+            log.info("Info sent to the user - " + username);
+        } else {
+            sendMessage(chatId, text); // echo bot
+            log.info("Replied to the user - " + username);
+        }
+    }
+
+    private void sendMessage(long chatId, String text) {
         SendMessage sendMessage = new SendMessage();
         sendMessage.setChatId(String.valueOf(chatId));
-        sendMessage.setText("Hi, your message was: " + text);
+        sendMessage.setText(text);
 
         try {
             execute(sendMessage);
         } catch (TelegramApiException e) {
-            throw new RuntimeException(e);
+            log.error("Sending message error: " + e.getMessage());
         }
+    }
+
+    private void sendInfo(long chatId, Update update) {
+        Message message = update.getMessage();
+        Chat chat = message.getChat();
+
+        String infoBuilder = "Information about received message update:\n" +
+                "CharId: " + message.getChatId() + "\n" +
+                "Text length: " + message.getText().length() + "\n" +
+                "Username: " + chat.getUserName() + "\n" +
+                "First name: " + chat.getFirstName() + "\n" +
+                "Last name: " + chat.getLastName() + "\n";
+
+        sendMessage(chatId, infoBuilder);
     }
 }
